29c8a74662698f4af38c5d14112942665ee36737
Ramadhan Wiedjayaprakoso <ramadhan.prakoso@datacomm.co.id>
2020-01-08 10:28:19 +0700

    first commit and finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';  
+import { DashboardComponent } from './dashboard/dashboard.component'; 
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'heroes', component: HeroesComponent},
+  {path : 'dashboard', component: DashboardComponent},
+  {path : 'detail/:id', component: HeroDetailComponent},
+  
+  //default path 
+  {path : '', redirectTo: '/dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
